06aee235be8aca3f31b7ee876792aeb2832fff0a
Peyton Cornelison <peytoncornelison@gmail.com>
2020-07-21 18:54:22 -0500

    added vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
