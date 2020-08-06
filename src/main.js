51fd2cfe876b1394bf330b330dbda24c18995f87
Aston Jones <astonbradleyjones@gmail.com>
2020-08-06 13:11:33 -0500

    got rid of bootstrap bootstrap-vue and added Vuetify.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import router from '@/router/index.js'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 
 // The SRC css file
 import '@/assets/css/main.css'
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-Vue.use(IconsPlugin)
+// Vuetify
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
+
+Vue.use(vuetify)
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
